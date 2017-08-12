8768f21f236600fc81bb6ad90712e6bc05981fca
Adriano Reis <reis.a@outlook.com>
2017-08-12 08:42:44 +0200

    Refactoring acceptance test

--- src/acceptanceTest/java/com/oocode/MainTest.java
+++ src/acceptanceTest/java/com/oocode/MainTest.java
@@ -1,20 +1,26 @@
 package com.oocode;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class MainTest {
     // do not run without changing "Ivan's Windows" in com.oocode.BodyBuilder to "test"
     // otherwise you have to pay for the order
     @Test
     public void testLargeOrder() throws Exception {
-        Main.main(new String[]{"123", "456", "789", "Churchill"});
+        Windows windows = new Windows(new String[]{"123", "456", "789", "Churchill"});
+        assertThat(windows.getServerMessage(), equalTo("Thank you \"test\" for your large order (q=789, w=119, h=453, toughened). Order not really placed - nothing to pay"));
     }
 
     @Test
     public void testSmallOrder() throws Exception {
-
-        Main.main(new String[]{"48", "36", "1", "Victoria"});
+        Windows windows = new Windows(new String[]{"48", "36", "1", "Victoria"});
+        assertThat(windows.getServerMessage(), equalTo("Thank you \"test\" for your order (q=1, w=46, h=33, plain). Order not really placed - nothing to pay"));
     }
 
         /* EXPECTED OUTPUT
