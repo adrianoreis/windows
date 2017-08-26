c6880af967eb6ea7e138483f38689624a8d2fa9a
Adriano Reis <reis.a@outlook.com>
2017-08-26 16:19:55 +0200

    Help message

--- src/main/java/com/oocode/Main.java
+++ src/main/java/com/oocode/Main.java
@@ -2,6 +2,10 @@ package com.oocode;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        if(args.length < 4){
+            System.out.println("4 parameters: width height quantity model");
+            System.exit(0);
+        }
         Windows windows = new Windows(args);
         windows.invoke();
         System.out.println(windows.getServerMessage());
